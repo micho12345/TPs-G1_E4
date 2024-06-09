e828554587288f3a6cefba2ff158fa6f0bd46b10
micho12345 <vvenier@itba.edu.ar>
2024-06-09 17:06:22 -0300

    Codigo de la triangular OK

--- TP3/Laticce/8bits/TP3_E4/source/TP3_E4/TriangularWaves.v
+++ TP3/Laticce/8bits/TP3_E4/source/TP3_E4/TriangularWaves.v
@@ -1,12 +1,14 @@
 
 
+
+
 module TriangularWave (
     input wire clk, // Reloj de entrada a 12 MHz
-    output reg [11:0] triangular_wave // Salida de 12 bits
+    output reg [11:0] triangular_wave // Salida de 12 bits	
 );
 
-    reg [11:0] count = 12'b100000000000; // Contador para la onda triangular
-    reg direction = 1; // Dirección del contador (1 para incrementar, 0 para decrementar)
+    reg [11:0] count = 12'b000000000000; // Contador para la onda triangular
+    reg direction = 1; // Direcci?n del contador (1 para incrementar, 0 para decrementar)
     
     // Divisor de frecuencia
     reg clk_divider = 0; // Contador para dividir la frecuencia del reloj
@@ -24,10 +26,10 @@ module TriangularWave (
 
     // Contador triangular
     always @(posedge slow_clk) begin
-        // Cambiar la dirección en los límites del contador de la onda
-        if (count == 12'b011111111110) begin
+        // Cambiar la direcci?n en los l?mites del contador de la onda
+        if (count == 12'b011111111100) begin				// Mayor a 7FB
             direction <= 0; // Cambiar a decrecimiento
-        end else if (count == 12'b100000000000) begin
+        end else if (count == 12'b100000000010) begin	// mayor a 802
             direction <= 1; // Cambiar a incremento
         end
 
